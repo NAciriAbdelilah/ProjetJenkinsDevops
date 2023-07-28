8ca0b72bd0b613925a22975271a52f25ff068c27
Abdelilah Naciri <abdelilah.naciri@outlook.com>
2023-07-28 15:46:13 +0100

    first commit

---
+++ src/test/java/com/example/ProjetJenkinsDevops/ProjetJenkinsDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProjetJenkinsDevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetJenkinsDevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
